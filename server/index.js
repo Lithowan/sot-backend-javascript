09bf74282d2a958673449aa05f739f5bd75f2630
Wanja Leuthold <leuthowanj@myvuw.ac.nz>
2018-05-28 18:42:23 +1200

    used express to statically get the website out of /public

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+//app.get('/hello', (req, res) => res.send('Hello World! Now with 100% more nodemon!!'));
 
 app.listen(3000, () => console.log('Our app is listening on port 3000!'));
 
